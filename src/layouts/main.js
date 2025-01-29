5a29a2ac5e9870ea7c8ed45f0e70706523fc982c
Oksana Turova <psdhtmlcss@mail.ru>
2025-01-29 18:35:12 +0400

    envybox scripts

--- src/layouts/main.js
+++ src/layouts/main.js
@@ -7,6 +7,13 @@ const maskOptions = {
   mask: '+{7}(000)000-00-00'
 };
 
+// Envybox
+ const Envy = {
+  CSS: '<link rel="stylesheet" href="https://cdn.envybox.io/widget/cbk.css">',
+  SCRIPT: '<script type="text/javascript" src="https://cdn.envybox.io/widget/cbk.js?wcb_code=1ea8163ad43792f2a6f7d35027feee07" charset="UTF-8" async></script>',
+  LINK: '<a href="#callbackwidget">Заказать звонок</a>'
+}
+
 const phoneInput = document.querySelector('.js-phone-input');
 const feedbackModalElement = document.getElementById('feedbackModal');
 
@@ -14,6 +21,12 @@ let mask;
 
 if (phoneInput) {
   mask = IMask(phoneInput, maskOptions);
+}
+
+if (screen.width > 1024) {
+  const body = document.querySelector('body');
+  body.insertAdjacentHTML('beforeend', Envy.CSS);
+  body.insertAdjacentHTML('beforeend', Envy.SCRIPT);
 }
 
 feedbackModalElement.addEventListener('show.bs.modal', evt => {
